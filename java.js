110d962f71c84a067ee242217d6ce964190da32f
Kaiky Matsumoto <47525176+koekoki@users.noreply.github.com>
2021-10-07 22:02:04 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -1,5 +1,6 @@
 var number = parseInt(Math.random() * 10);
 var tentativas = 3;
+var gold = 0;
 function submitenter(myfield,e)
 {
 var keycode;
@@ -15,22 +16,41 @@ return false;
 else
 return true;
 }
+function reiniciar(){
+    tentativas = 3;
+    acertou = false;
+    document.getElementById("tt").innerHTML = "Advinhe o número do mago!";
+    document.getElementById("exibir2").innerHTML = "";
+    document.getElementById("exibir").innerHTML = "";
+    document.querySelector("#botao").style.display = "";
+    document.querySelector("#chute").disabled = false;
+    document.getElementById("moedas").style.display = "none";
+    document.getElementById("sapo").style.display = "none";
+    document.getElementById("chute").value='';
+    number = parseInt(Math.random() * 10);
+}
 
 function tentativa() {
     var acertou = false;
     var chute = parseInt(document.getElementById("chute").value);
-    console.log(chute, number, tentativas);
+    
+
     if (chute == number) {
+        document.getElementById("moedas").style.display = "";
         document.getElementById("exibir2").innerHTML = "Parabéns! Você acertou.";
         document.getElementById("exibir").innerHTML = "Pegue seus bitcoins!";
-        document.getElementById("tt").innerHTML = "<a href = 'https://koekoki.github.io/jogoDoMaguinho/'> tentar novamente <img class='reiniciar' src = 'https://image.flaticon.com/icons/png/512/82/82004.png'> </img> </a>";
+        document.getElementById("tt").innerHTML = "<a onclick = 'reiniciar()'> tentar novamente <img class='reiniciar' src = 'https://image.flaticon.com/icons/png/512/82/82004.png'> </img> </a>";
         document.getElementById("moedas").className = "moedas";
         document.getElementById("moedas").src = "https://imagensemoldes.com.br/wp-content/uploads/2020/09/Desenho-Moedas-Dinheiro-Bitcoin-PNG.png";
         acertou = true;
         var audio = new Audio('css/vitoria.mp3');
         document.querySelector("#chute").disabled = true;
+        gold = gold + 1;
+        chute = 0;
+        document.getElementById("gold").innerHTML = gold; 
         audio.addEventListener('canplaythrough', function() {
         audio.play();
+        return false;
 });
         document.querySelector("#botao").style.display = "none";
     } else if (chute > number) {
@@ -43,9 +63,10 @@ function tentativa() {
         atualizaTentativas(tentativas)
     }
     if (tentativas == 0 && acertou == false) {
+        document.getElementById("sapo").style.display = "";
         document.getElementById("exibir2").innerHTML = "suas tentativas acabaram!";
         document.getElementById("exibir").innerHTML = "vire sapo!";
-        document.getElementById("tt").innerHTML = "<a href = 'https://koekoki.github.io/jogoDoMaguinho/'> tentar novamente  <img class='reiniciar' src = 'https://image.flaticon.com/icons/png/512/82/82004.png'> </img> </a>";
+        document.getElementById("tt").innerHTML = "<a onclick = 'reiniciar()' > tentar novamente  <img class='reiniciar' src = 'https://image.flaticon.com/icons/png/512/82/82004.png'> </img> </a>";
         document.querySelector("#botao").style.display = "none";
         document.getElementById("sapo").className = "sapo";
         document.getElementById("sapo").src = "https://cdn-0.emojis.wiki/emoji-pics/facebook/frog-facebook.png";
